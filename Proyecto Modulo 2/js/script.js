b41ca5319c44fc010e334d00ca293da619b0a825
dcastle-c <davidcastillo422@outlook.com>
2026-01-25 13:21:57 -0300

    Implementación logica de depósitos

--- Proyecto Modulo 2/js/script.js
+++ Proyecto Modulo 2/js/script.js
@@ -1,13 +1,41 @@
 $(document).ready(function() {
+    let saldoActual = 1250000;
+
     $('#btnLogin').on('click', function(e) {
         const email = $('#email').val();
         const password = $('#password').val();
 
         if (email === "" || password === "") {
-            e.preventDefault(); 
+            e.preventDefault();
             alert("Por favor, complete todos los campos para ingresar al castillo.");
+        }
+    });
+
+    $('#depositForm').on('submit', function(e) {
+        e.preventDefault(); 
+        
+        const monto = parseFloat($('#amount').val());
+        
+        if (monto > 0) {
+            saldoActual += monto;
+            alert("¡Ingreso exitoso! Su nuevo saldo es: $" + saldoActual.toLocaleString('es-CL'));
+            window.location.href = 'menu.html'; 
         } else {
-            console.log("Acceso concedido para: " + email);
+            alert("Por favor, ingrese un monto válido.");
+        }
+    });
+
+    $('#transferForm').on('submit', function(e) {
+        e.preventDefault();
+        
+        const montoEnviado = parseFloat($('#sendAmount').val());
+        
+        if (montoEnviado > 0 && montoEnviado <= saldoActual) {
+            saldoActual -= montoEnviado; /
+            alert("Transferencia realizada con éxito.");
+            window.location.href = 'menu.html';
+        } else if (montoEnviado > saldoActual) {
+            alert("Error: Fondos insuficientes en la bóveda.");
         }
     });
 });
